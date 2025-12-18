0a18ae51a53aa9c29142072f55dabcf44852cb38
ArrowTin <tinasandrianus@gmail.com>
2025-12-18 07:32:01 +0700

    sv

--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::middleware([EnsureIsAdmin::class])->group(function () {
     Route::post('/listBook/delete', [AdminController::class, 'deleteBook'])->name('book.delete');
     Route::get('/listBook/delete', function () {
         return redirect()->route('book.list');
-    })->name('book.delete');
+    });
 
     Route::get('/peminjaman/update', [AdminController::class, 'showPeminjamanPage'])->name('peminjaman.update');
     Route::post('/peminjaman/update', [AdminController::class, 'peminjamanUpdate']);
